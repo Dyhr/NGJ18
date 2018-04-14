b5ab1e48fd267f95f3497708cff6db2391aa4d69
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2018-04-14 06:26:47 +0200

    Initial commit

---
+++ Server/main.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(`
+  <!DOCTYPE \"html\">
+  <html>
+  <head>
+  <title>Hello World Page</title>
+  </head>
+  <body>
+  Hello World!
+  </body>
+  </html>`);
+  response.end();
+});
+
+server.listen(80);
+console.log("Server is listening");
