7f7c29402edeafb116dcdc6358fd8c6f62067109
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2018-04-14 07:18:46 +0200

    Updating node

--- Server/run.sh
+++ Server/run.sh
@@ -3,4 +3,10 @@
 killall node
 sudo yum -y update
 sudo yum -y install nodejs npm --enablerepo=epel
+
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+node -v
+
 node /webapps/ngj18/main.js
