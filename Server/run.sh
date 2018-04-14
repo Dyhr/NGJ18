bed21b59869092b972afee20574024fd6072dc91
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2018-04-14 09:20:11 +0200

    Manually added npm packages

--- Server/run.sh
+++ Server/run.sh
@@ -5,8 +5,8 @@ sudo yum -y update
 sudo yum -y install nodejs npm --enablerepo=epel
 node -v
 
-npm install bind
-npm install rwlock
-npm install querystring
+#npm install bind
+#npm install rwlock
+#npm install querystring
 
 nohup node /webapps/ngj18/main.js &>/dev/null &
