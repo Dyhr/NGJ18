9954f2c244aec802a84edcb033ede62e094d4d79
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2018-04-14 07:23:29 +0200

    Fixed the error

--- Server/main.js
+++ Server/main.js
@@ -1,19 +1,19 @@
 var http = require("http");
 var server = http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(`
-  <!DOCTYPE \"html\">
-  <html>
-  <head>
-  <title>Game Controller</title>
-  <style>
-
-  </style>
-  </head>
-  <body>
-  Hello World!
-  </body>
-  </html>`);
+  response.write(" \
+  <!DOCTYPE \"html\"> \
+  <html> \
+  <head> \
+  <title>Game Controller</title> \
+  <style> \
+          \
+  </style> \
+  </head> \
+  <body> \
+  Hello World! \
+  </body> \
+  </html>");
   response.end();
 });
 
