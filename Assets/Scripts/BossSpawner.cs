8476aa8722f28f7517103ae6642fbd01b156a813
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2018-04-14 18:56:29 +0200

    Sweet assets

---
+++ Assets/Scripts/BossSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSpawner : MonoBehaviour {
+
+	public GameObject BossPrefab;
+	public GameObject MinionPrefab;
+
+	public int NumberOfMinions;
+
+	private bool spawning;
+	
+	private void Update () {
+		if (Input.GetKey(KeyCode.Space) && !spawning) {
+			spawning = true;
+			StartCoroutine(Spawn());
+		}
+	}
+
+	private IEnumerator Spawn() {
+		Debug.Log("Starting...");
+		Instantiate(BossPrefab, transform.position, Quaternion.Euler(0,180,0));
+
+		for (int i = 0; i < NumberOfMinions; i++) {
+			yield return new WaitForSeconds(Random.Range(0.1f,0.4f));
+			Instantiate(MinionPrefab, transform.position + new Vector3(Random.Range(-8f,8f),0,Random.Range(-2f,3f)),
+				Quaternion.Euler(0, Random.Range(0f, 360f), 0));
+		}
+			
+		Destroy(this);
+	}
+}
